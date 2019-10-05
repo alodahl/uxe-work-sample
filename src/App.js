033b761aa71b454701b0c04accce7f1d3a283d00
Lodahl, Alina <alina_lodahl@gap.com>
2019-10-05 10:56:29 -0700

    refactor: abstract controls into their own sections, update react to use hooks and pure functions

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import getControls from './api'
 import { Redirect, Route, BrowserRouter as Router } from 'react-router-dom'
-import ControlList from './components/ControlList';
-import ControlMain from './components/ControlMain';
 import Header from './components/Header'
 import FilterBar from './components/FilterBar'
 import Controls from './components/Controls'
 
-class App extends Component {
-  state = {
-    controls: null
-  }
-
-  componentDidMount() {
-    // We have provided a simple getControls() API that will load controls.json
-    // for you.  getControls() imposes an artificial delay of 1500ms.
-    getControls().then(controls => this.setState({ controls }))
-  }
-
-  render() {
-    const { controls } = this.state;
+const App = () => {
 
     return (
       <Router>
         <div className="App">
-        <Header/>
-        <FilterBar/>
-        <Controls>
-          { controls &&
-            <>
-              <ControlList controls={controls} />
-
-              <Route exact path='/'render={() => (
-                <Redirect to={{ pathname: `/controls/${controls[0].id}` }} />
-              )} />
-
-              <Route path='/controls/:controlId' render={({ match }) => (
-                <ControlMain control={controls.find(c => c.id.toString() === match.params.controlId)} />
-              )} />
-            </>
-          }
-        </Controls>
+          <Header/>
+          <FilterBar/>
+          <Controls/>
         </div>
       </Router>
     );
-  }
 }
 
 export default App;
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import getControls from '../api'
+import { Redirect, Route, BrowserRouter as Router } from 'react-router-dom'
+import ControlList from './ControlList';
+import ControlMain from './ControlMain';
 
-function Controls(props) {
+const sectionStyle = { backgroundColor: '#fff' };
+
+const controlMainStyle = { ...sectionStyle, backgroundColor: '#ccc' };
+
+const Controls = props => {
+  const [controls, setControls] = useState(null);
+  useEffect(() => {
+    // We have provided a simple getControls() API that will load controls.json
+    // for you.  getControls() imposes an artificial delay of 1500ms.
+    getControls().then(controls => setControls(controls));
+  })
+
   return (
-      <div className="controls">
-        <h2>
-          Controls
-        </h2>
-        {props.children}
+      <div className="controls" >
+        { controls &&
+          <>
+            <section style={sectionStyle}>
+              <h2>
+                Controls
+              </h2>
+              <ControlList controls={controls} />
+              <Route exact path='/'render={() => (
+                <Redirect to={{ pathname: `/controls/${controls[0].id}` }} />
+              )} />
+            </section>
+            <section style={controlMainStyle}>
+              <Route path='/controls/:controlId' render={({ match }) => (
+                <ControlMain control={controls.find(c => c.id.toString() === match.params.controlId)} />
+              )} />
+            </section>
+          </>
+        }
       </div>
   );
 }
